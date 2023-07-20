6977282c9b56d687c5bf13c52d264cd1256baca6
iamsidh <siddheshmadgaonkar@gmail.com>
2023-07-20 21:01:45 +0530

    Telegram API token and userid read from properties file

--- src/ipChecker/TelegramAPIBot.java
+++ src/ipChecker/TelegramAPIBot.java
@@ -1,22 +1,24 @@
 package ipChecker;
 
-import java.io.File;
+//import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.Properties;
+//import java.util.Scanner;
 
 public class TelegramAPIBot {
 
 	public static void sendTelegram(String publicip) throws IOException {
 
 		String command = String.format(
-				"curl -X POST https://api.telegram.org/bot%s/sendMessage?chat_id=496409188&text=New_Public_IP=%s",getToken(),
-				publicip);
+				"curl -X POST https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=New_Public_IP=%s",getCredentials("token"),getCredentials("userid"),
+				IPChecker.getPublicIP());
 		Process process = Runtime.getRuntime().exec(command);
 		process.getInputStream();
 	}
 
-	private static String getToken() {
+	/*private static String getToken() {
 
 		 String token = null;
 
@@ -42,6 +44,26 @@ public class TelegramAPIBot {
 			}
 		}
 		return null;
+	}*/
+	
+	private static String getCredentials(String a) {
+		
+		Properties prop = new Properties();
+
+
+        FileInputStream fis = null;
+		try {
+			fis = new FileInputStream("C:\\Sidh\\ipchecker\\credentials.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+        try {
+			prop.load(fis);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        return prop.getProperty(a);
 	}
 
 }
